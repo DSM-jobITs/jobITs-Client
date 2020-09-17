9d72e4355281b48b7a33dbfa8834429250780b8f
Giljun Bae <bgj0127@gmail.com>
2020-09-17 16:05:11 +0900

    [FIX] webpack 수정

--- webpack.config.js
+++ webpack.config.js
@@ -34,12 +34,38 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(jpg|jpeg|gif|png|svg|ico)?$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 10000,
+              fallback: "file-loader",
+              name: "images/[name].[ext]",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 10000,
+              fallback: "file-loader",
+              name: "fonts/[name].[ext]",
+            },
+          },
+        ],
+      },
     ],
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
-      template: "public/index.html",
+      template: "dist/index.html",
     }),
   ],
   devServer: {
