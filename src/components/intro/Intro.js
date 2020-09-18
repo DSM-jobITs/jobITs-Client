e089dbfd8318c6d3cec67339cf9b808c735a410c
Giljun Bae <bgj0127@gmail.com>
2020-09-18 23:47:43 +0900

    [ADD] <Intro.js> CSS 수정

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -6,7 +6,12 @@ const Intro = () => {
     <S.MainWrapper>
       <S.IntroImg src="/src/img/IntroImg.png" />
       <S.TextWarpper>
-        <div>hello</div>
+        <S.FirstText>대마고 학생들을 위한</S.FirstText>
+        <S.MiddleText>기업 정보 소개 사이트</S.MiddleText>
+        <S.LastText>
+          더 이상 취업 고민은 그만! 자비스에서 여러분들에게 취업과
+          <br /> 관련한 많은 정보를 제공해드립니다.
+        </S.LastText>
       </S.TextWarpper>
     </S.MainWrapper>
   );
--- src/components/intro/style.js
+++ src/components/intro/style.js
@@ -14,8 +14,28 @@ export const IntroImg = styled.img`
 
 export const TextWarpper = styled.div`
   display: flex;
-  width: 40%;
   height: 200px;
+  margin: 40px;
   flex-direction: column;
-  background: #000000;
+`;
+
+export const FirstText = styled.p`
+  font-size: 1.5rem;
+  font-weight: bold;
+  margin-bottom: 10px;
+`;
+
+export const MiddleText = styled.p`
+  font-size: 3rem;
+  font-weight: bold;
+  margin: 0;
+  margin-bottom: 20px;
+`;
+
+export const LastText = styled.p`
+  font-size: 1rem;
+  font-weight: bold;
+  color: #a1a1a1;
+  margin: 0;
+  margin-bottom: 10px;
 `;
