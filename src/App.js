95f51efe3f86878afbec74de4368d66ade042762
Giljun Bae <bgj0127@gmail.com>
2020-09-19 21:47:36 +0900

    [ADD] Container 이용

--- src/App.js
+++ src/App.js
@@ -3,21 +3,13 @@ import { hot } from "react-hot-loader";
 import { BrowserRouter } from "react-router-dom";
 import Routing from "./Routing";
 import { GlobalStyle } from "./GlobalStyle";
-import Header from "./components/navLink/Header";
-import Login from "./components/main/login/Login";
-import NoticePreview from "./components/main/preview/NoticePreview";
-import Intro from "./components/main/intro/Intro";
-import Footer from "./components/main/footer/Footer";
+import MainContainer from "./containers/MainContainer";
 
 const App = () => {
   return (
     <BrowserRouter>
       <GlobalStyle />
-      <Header />
-      <Login />
-      <NoticePreview />
-      <Intro />
-      <Footer />
+      <MainContainer />
     </BrowserRouter>
   );
 };
