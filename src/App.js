3ced6d8287c5beaaa7893236d9f6f27510a072a0
Giljun Bae <bgj0127@gmail.com>
2020-09-19 18:28:14 +0900

    [CREATE] Container 생성

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import { BrowserRouter } from "react-router-dom";
 import Routing from "./Routing";
 import { GlobalStyle } from "./GlobalStyle";
 import Header from "./components/navLink/Header";
-import Login from "./components/login/Login";
-import NoticePreview from "./components/preview/NoticePreview";
-import Intro from "./components/intro/Intro";
-import Footer from "./components/footer/Footer";
+import Login from "./components/main/login/Login";
+import NoticePreview from "./components/main/preview/NoticePreview";
+import Intro from "./components/main/intro/Intro";
+import Footer from "./components/main/footer/Footer";
 
 const App = () => {
   return (
