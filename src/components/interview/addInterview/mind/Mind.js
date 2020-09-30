fe721f9ff7e5f1bb4f912a2d8cd8010fc9b23a29
Giljun Bae <bgj0127@gmail.com>
2020-09-30 23:13:24 +0900

    [ADD] <AddInterview> 질문 추가
    아직 못했다

--- src/components/interview/addInterview/mind/Mind.js
+++ src/components/interview/addInterview/mind/Mind.js
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Question from "../question/Question";
 import * as S from "./style";
 
 const Mind = () => {
+  const [add, setAdd] = useState(false);
+
+  const ClickButton = e => {
+    e.preventDefault();
+  };
+
+  const onAdd = () => {
+    setAdd(prev => !prev);
+  };
+
   return (
     <S.MainWarpper>
       <S.Title>
         <S.TitleText>인성 면접 질문</S.TitleText>
         <S.TitleSubmit type="submit">등록하기</S.TitleSubmit>
       </S.Title>
-      <S.AddBox>
-        <Question></Question>
-      </S.AddBox>
-      <S.AddQuestion>질문 추가하기</S.AddQuestion>
+      <S.AddBox>{add && <Question />}</S.AddBox>
+      <S.AddQuestion onClick={onAdd}>질문 추가하기</S.AddQuestion>
     </S.MainWarpper>
   );
 };
