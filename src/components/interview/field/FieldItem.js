92b60aaaefce00d388e581d4a31c7805dee428c9
Giljun Bae <bgj0127@gmail.com>
2020-10-14 09:10:26 +0900

    [FIX] <FieldItem> radio 선택

--- src/components/interview/field/FieldItem.js
+++ src/components/interview/field/FieldItem.js
@@ -1,27 +1,15 @@
 import React from "react";
-import styled from "styled-components"
-
-const Label = styled.label`
-  display: flex;
-  align-items:center;
-  justify-content:center;
-  min-width: 200px;
-  height: 60px;
-  border: none;
-  border-radius: 15px;
-  background: #ffffff;
-  box-shadow: 1px 2px 4px -1px rgba(0, 0, 0, 0.3);
-  margin: 25px;
-  cursor: pointer;
-  :checked{
-    background-color: #000000;
-`
+import * as S from "./ItemStyle";
 
 const FieldItem = ({field}) => {
+
+  const onClick = e =>{
+    const {value} = e.target;
+  }
   return (
     <>
-      <input name="field" type="radio" id={field} hidden/>
-      <Label for={field}>{field}</Label>
+      <S.Input onClick={onClick} name="field" type="radio" id={field} hidden/>
+      <S.Label for={field}>{field}</S.Label>
     </>
   );
 };
---
+++ src/components/interview/field/ItemStyle.js
@@ -0,0 +1,22 @@
+import styled from "styled-components"
+
+export const Label = styled.label`
+display: flex;
+align-items:center;
+justify-content:center;
+min-width: 200px;
+height: 60px;
+border: none;
+border-radius: 15px;
+background: #ffffff;
+box-shadow: 1px 2px 4px -1px rgba(0, 0, 0, 0.3);
+margin: 25px;
+cursor: pointer;
+`
+export const Input = styled.input`
+&:checked + label{
+  color: #ffffff;
+  background: #bb45ff;
+  font-weight:bold;
+}
+`
