a89999d790024ff274ba7819bde0153df25a22e6
Srim <deamoon0801@naver.com>
2020-10-28 20:22:23 +0900

    [ADD] <Company/style> style

--- src/components/company/style.js
+++ src/components/company/style.js
@@ -1,33 +1,37 @@
-import styled from "styled-components"
-
-export const Warpper = styled.div`
-  display: flex;
-  width: 100%;
-  min-height: 100vh;
-  background:#F6F6F6;
-  flex-direction:column;
-  justify-content:flex-start;
-  align-items:center;
-`
-
-export const Header = styled.div`
-  margin-top: 70px;
-  display: flex;
-  width: 70%;
-  height: 100px;
-  align-items:center;
-`
-
-export const Title = styled.p`
-  width: 300px;
-  font-size:2rem;
-  font-weight: bold;
-  text-align:center;
-  margin-left:-80px;
-`
+import styled from "styled-components";
 
 export const CompanysWarpper = styled.div`
   width: 70%;
   height: 100vh;
-  background: #af3a3f;
+  border: 1px solid black;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Container = styled.div`
+  width : 23%;
+  height : 30%;
+`;
+
+export const ImgContainer = styled.img`
+  width: 100%;
+  height: 80%;
+  background-color: red;
+`;
+
+export const CompanysTitle = styled.p`
+  text-align: center;
+`;
+
+export const Title = styled.h1`
+  width: 100%;
+  font-size: 1.5rem;
+  font-weight: bold;
+`;
+
+export const Header = styled.div`
+  margin-top: 70px;
+  margin : 1rem 30rem 0rem 30rem;
+  width: 70%;
+  height: 100px;
 `
