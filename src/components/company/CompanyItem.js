a5cb0c347a11c4abaad2c4df9e5f028eb798e32e
Srim <deamoon0801@naver.com>
2020-10-28 20:22:04 +0900

    [ADD] <CompanyItem> UI

---
+++ src/components/company/CompanyItem.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import * as C from "./style";
+
+const CompanyItem = () => {
+    return (
+        <C.Container>
+            <C.ImgContainer src=""></C.ImgContainer>
+
+            <C.CompanysTitle>카카오톡</C.CompanysTitle>
+        </C.Container>
+    );
+};
+
+export default CompanyItem;
