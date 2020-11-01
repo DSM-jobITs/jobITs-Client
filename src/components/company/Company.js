1f94409bc490f4e2ec4164e6d169977d0bc2b380
Srim <deamoon0801@naver.com>
2020-11-01 17:13:04 +0900

    [MODIFY] <Company> style

--- src/components/company/Company.js
+++ src/components/company/Company.js
@@ -5,16 +5,16 @@ import CompanyItem from "./CompanyItem";
 
 const Company = () => {
   return (
-    <S.Warpper>
+    <S.Warpper company>
       <h1>ㅎㅇ</h1>
       <C.Header>
         <C.Title>기업 모두 보기</C.Title>
       </C.Header>
       <C.CompanysWarpper>
-        <CompanyItem></CompanyItem>
-        <CompanyItem></CompanyItem>
-        <CompanyItem></CompanyItem>
-        <CompanyItem></CompanyItem>
+          <CompanyItem></CompanyItem>
+          <CompanyItem></CompanyItem>
+          <CompanyItem></CompanyItem>
+          <CompanyItem></CompanyItem>
       </C.CompanysWarpper>
     </S.Warpper>
   );
