8a333a6fb5f0ae8b4b01b3ed9c73a5adc36472e9
Srim <deamoon0801@naver.com>
2020-11-01 17:12:30 +0900

    [ADD] <CompanyItem> Link

--- src/components/company/CompanyItem.js
+++ src/components/company/CompanyItem.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React from "react";
 import * as C from "./style";
+import { Link } from "react-router-dom";
 
-const CompanyItem = () => {
-    return (
-        <C.Container>
-            <C.ImgContainer src=""></C.ImgContainer>
-
-            <C.CompanysTitle>카카오톡</C.CompanysTitle>
-        </C.Container>
-    );
+const CompanyItem = ({ id }) => {
+  return (
+    <C.Container>
+      <Link
+        style={{ textDecoration: "none" }}
+        to={{
+          pathname: `/company/${id}`,
+        }}
+      >
+        <C.ImgContainer src=""></C.ImgContainer>
+      </Link>
+      <C.CompanysTitle>카카오톡</C.CompanysTitle>
+    </C.Container>
+  );
 };
 
 export default CompanyItem;
