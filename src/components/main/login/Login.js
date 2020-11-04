9397aac300cdad95eb9cc67c1d41a7e2a5b74a7c
Giljun Bae <bgj0127@gmail.com>
2020-11-04 19:59:37 +0900

    [FIX] <Login> 로그인 시 화면 전환

--- src/components/main/login/Login.js
+++ src/components/main/login/Login.js
@@ -3,11 +3,12 @@ import LoginBox from "./loginBox/LoginBox";
 import MainBox from "./MainBox/MainBox";
 import * as S from "./style";
 
-const Login = () => {
+const Login = ({isLogin, setIsLogin}) => {
+  {console.log(isLogin)}
   return (
     <S.ContentWarp>
-      {/* <LoginBox /> */}
-      <MainBox />
+    {isLogin ?
+      <MainBox /> : <LoginBox setIsLogin={setIsLogin} /> }
     </S.ContentWarp>
   );
 };
