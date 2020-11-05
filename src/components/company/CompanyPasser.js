e8d9d3eb0126afdf51aefd54c5b924253a79767f
Srim <deamoon0801@naver.com>
2020-11-05 17:12:14 +0900

    [ADD] <CompanyPasser> view

--- src/components/company/CompanyPasser.js
+++ src/components/company/CompanyPasser.js
@@ -1,10 +1,14 @@
 import React from "react";
 import * as C from "./style";
 
-const CompanyPasser = ({year, num}) => {
-  return <C.DetailContainer>
-      <C.PasserText first>{year}년 / {num}명 합격</C.PasserText>
-  </C.DetailContainer>;
+const CompanyPasser = ({ year, num }) => {
+  return (
+    <C.DetailContainer>
+      <C.PasserText first>
+        {year}년 / {num}명 합격
+      </C.PasserText>
+    </C.DetailContainer>
+  );
 };
 
 export default CompanyPasser;
