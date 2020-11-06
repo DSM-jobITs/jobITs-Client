8c62e5b927b9b76dd27dd755a724ec542cbda81b
Giljun Bae <51225580+bgj0127@users.noreply.github.com>
2020-11-06 18:47:48 +0900

    Update Employment.js

--- src/components/employment/Employment.js
+++ src/components/employment/Employment.js
@@ -8,9 +8,7 @@ import { baseUrl } from "../../constant/index"
 
 const Employment = () => {
   const [contents, setContents] = useState([]);
-  const config = {
-    headers : { "Authorization" : "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IkFkbWluIiwiaWF0IjoxNjA0NDgzMTYwLCJleHAiOjE2MTMxMjMxNjB9.DESIU01OzkbR5jxt7yOiavfNQ_6O-8x9da8PweStCSk"}
-  };
+  
 
   useEffect(()=>{ 
     axios.get(baseUrl + "employment",config)
