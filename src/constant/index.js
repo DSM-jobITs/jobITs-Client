c0875ada46aa1c707da9a366c5bfd4cad0e056b8
Giljun Bae <bgj0127@gmail.com>
2020-11-06 19:39:43 +0900

    [FIX] gitignore 수정

--- src/constant/index.js
+++ src/constant/index.js
@@ -1,1 +1,4 @@
 export const baseUrl = "http://13.125.175.100:3000/";
+export const config = {
+  headers : { "Authorization" : "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IkFkbWluIiwiaWF0IjoxNjA0NDgzMTYwLCJleHAiOjE2MTMxMjMxNjB9.DESIU01OzkbR5jxt7yOiavfNQ_6O-8x9da8PweStCSk"}
+};
