f4a4727bf407692446bdb2865bb8b205e3d31414
Srim <deamoon0801@naver.com>
2020-11-08 20:07:13 +0900

    [ADD] <NoticeDetailView> file download feature

--- src/components/notice/NoticeDetailView.js
+++ src/components/notice/NoticeDetailView.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import * as S from "../employment/style"
-import * as N from "./detailStyle"
-import { Link, useHistory } from "react-router-dom"
+import * as S from "./addNotice/style";
+import * as N from "./detailStyle";
+import { Link, useHistory } from "react-router-dom";
 import axios from 'axios';
 import { baseUrl } from '../../constant';
 
@@ -29,7 +29,7 @@ const NoticeItem = ({location, id}) => {
 
   return (
     <S.Warpper>
-      <S.Header>
+      <S.Header detail>
         <S.Title>공지사항</S.Title>
         <N.Button onClick={onNoticeHistory}>목록으로</N.Button>
       </S.Header>
@@ -42,6 +42,7 @@ const NoticeItem = ({location, id}) => {
       </N.Viewer>
       <N.FileBox>
         <N.P>첨부파일</N.P>
+        <div>{files.map((f) => (<div style={{marginLeft : "1.5rem"}}><a style={{textDecoration : "none"}} href={f.url} key={f.id} download={f.filename}>{f.filename}</a></div>))}</div>
       </N.FileBox>
     </S.Warpper>
   );
