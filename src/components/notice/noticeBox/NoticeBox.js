147af2b4f6c5a994299d5d2c2fcc732f469acf84
Srim <deamoon0801@naver.com>
2020-11-10 23:29:03 +0900

    [ADD] <NoticeBox> 공지 삭제

--- src/components/notice/noticeBox/NoticeBox.js
+++ src/components/notice/noticeBox/NoticeBox.js
@@ -1,9 +1,32 @@
 import React from "react";
 import * as S from "./style";
 import * as N from "../style";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { baseUrl } from "../../../constant";
 
 const NoticeBox = ({title, createdAt, id}) => {
+
+  const config = {
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IkFkbWluIiwiaWF0IjoxNjA0NDgzMTYwLCJleHAiOjE2MTMxMjMxNjB9.DESIU01OzkbR5jxt7yOiavfNQ_6O-8x9da8PweStCSk",
+    },
+  };
+
+  const onDeleteNotice = () => {
+    axios.delete(baseUrl + "notice/" + id, config)
+    .then((res) => {
+      console.log(res);
+      setTimeout(() => {
+        window.location.reload();
+      }, 500);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }
+
   return (
     <S.List>
       <Link
@@ -18,7 +41,7 @@ const NoticeBox = ({title, createdAt, id}) => {
       </S.ListContent>
       </Link>
       <S.ListDate>{createdAt}</S.ListDate>
-      <S.DeleteButton>삭제</S.DeleteButton>
+      <S.DeleteButton onClick={onDeleteNotice}>삭제</S.DeleteButton>
     </S.List>
   );
 };
