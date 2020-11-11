21644f27decc1c6db46026f0c34d96cc82524f37
Srim <deamoon0801@naver.com>
2020-11-11 18:30:38 +0900

    [MODIFY] <main/style> style

--- src/components/main/preview/style.js
+++ src/components/main/preview/style.js
@@ -2,8 +2,7 @@ import styled from "styled-components";
 
 export const MainWrapper = styled.div`
   width: 100%;
-  height: 25rem;
-  overflow: hidden;
+  height: 30rem;
   background: #fff;
   display: flex;
   justify-content: center;
