a37ae09a63ea402d62f70519bb3e0a52789b8668
Srim <deamoon0801@naver.com>
2020-11-12 20:17:52 +0900

    [ADD] <NoticeBox> admin 구분

--- src/components/notice/noticeBox/NoticeBox.js
+++ src/components/notice/noticeBox/NoticeBox.js
@@ -5,18 +5,18 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 import { baseUrl } from "../../../constant";
 
-const NoticeBox = ({title, createdAt, id}) => {
+const NoticeBox = ({title, createdAt, id, isAdmin}) => {
 
   const config = {
-    headers: {
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IkFkbWluIiwiaWF0IjoxNjA0NDgzMTYwLCJleHAiOjE2MTMxMjMxNjB9.DESIU01OzkbR5jxt7yOiavfNQ_6O-8x9da8PweStCSk",
-    },
-  };
+		headers: {
+			Authorization:
+				"Bearer " + localStorage.getItem("token"),
+		},
+	};
 
   const onDeleteNotice = () => {
     axios.delete(baseUrl + "notice/" + id, config)
-    .then((res) => {
+    .then(() => {
       alert("해당 공지가 삭제되었습니다.");
       setTimeout(() => {
         window.location.reload();
@@ -41,7 +41,7 @@ const NoticeBox = ({title, createdAt, id}) => {
       </S.ListContent>
       </Link>
       <S.ListDate>{createdAt}</S.ListDate>
-      <S.DeleteButton onClick={onDeleteNotice}>삭제</S.DeleteButton>
+      <S.DeleteButton onClick={onDeleteNotice} style={{display : isAdmin? "" : "none"}}>삭제</S.DeleteButton>
     </S.List>
   );
 };
