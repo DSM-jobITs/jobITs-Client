02b2625d4dbe8cee40cb91c0273287baf1e650ec
Giljun Bae <51225580+bgj0127@users.noreply.github.com>
2021-03-15 13:16:28 +0900

    Update AddEmployment.js

--- src/components/employment/addEmployment/AddEmployment.js
+++ src/components/employment/addEmployment/AddEmployment.js
@@ -9,7 +9,7 @@ const AddEmployment = () => {
   const [question,setQuestion] = useState("");
   const [answer,setAnswer] = useState("");
   const config = {
-    headers : { "Authorization" : "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IkFkbWluIiwiaWF0IjoxNjA0NDgzMTYwLCJleHAiOjE2MTMxMjMxNjB9.DESIU01OzkbR5jxt7yOiavfNQ_6O-8x9da8PweStCSk"}
+   
   };
 
   const handleSubmit = (e) => {
